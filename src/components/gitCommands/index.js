5c215fdc2470e554663f9df4415de5d32657a32e
ganesh.khaire@knorex.com <ganesh.khaire@knorex.com>
2019-09-30 09:06:36 +0530

    temp commit

--- src/components/gitCommands/index.js
+++ src/components/gitCommands/index.js
@@ -2,52 +2,108 @@ import React, { Component } from 'react';
 import "./index.scss";
 import exportsJson from "../helper";
 
+import utils from "../../utils";
+
 class GitCommands extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      l1Value: "0",
+      l2Value: "0",
+      l3Value: "0",
+      command: "",
+      psScript: ""
+    }
+  }
+
+
   render() {
-    console.log('exportsJson.l1Json', exportsJson.l1Json);
-    console.log('exportsJson.l12Json', exportsJson.l2Json);
-    console.log('exportsJson.l3Json', exportsJson.l3Json);
     return (
       <div className="row">
         <div className="col-4 ">
-          <h1 className="">Commands Explorer :- </h1>
-          Select the correct action you wish to
-          perform from the drop down below and
-          we will tell how to do it.
-          <div className="pl-5 pr-5 mt-5">
+          <div>
+            <h1 className="">Commands Explorer :- </h1>
+
+          </div>
+          <div>
+            <p>Select the correct action you wish to
+              perform from the drop down below and
+              we will tell how to do it.
+            </p>
+          </div>
+          <div className="pl-3 pr-3">
             <p className="text-pr-1 font-weight-bold">I want to :</p>
             <div className="form-group">
-              <select className="custom-select" id="inputGroupSelect01">
-                <option selected>Choose...</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+              <select value={this.state.l1Value} className="custom-select" id="l1Json" onChange={this.l1Changed}>
+                <option value="0" disabled>Choose...</option>
+                {exportsJson.l1Json.map((elm) => {
+                  const label = elm.label.charAt(0).toUpperCase() + elm.label.slice(1);
+                  return (<option key={utils.getUUID()} value={elm.value}>{label}</option>)
+                })}
               </select>
             </div>
             <div className="form-group">
-              <select className="custom-select" id="inputGroupSelect01">
-                <option selected>Choose...</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+              <select value={this.state.l2Value} className="custom-select" id="l2Json" onChange={this.l2Changed}>
+                <option value="0" disabled>Choose...</option>
+                {this.state.l1Value === "0" || exportsJson.l2Json[this.state.l1Value].map((elm) => {
+                  const label = elm.label.charAt(0).toUpperCase() + elm.label.slice(1);
+                  return (<option key={utils.getUUID()} value={elm.value}>{label}</option>)
+                })}
               </select>
             </div>
-            <div className="form-group">
-              <select className="custom-select" id="inputGroupSelect01">
-                <option selected>Choose...</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+            {(exportsJson.l3Json[this.state.l2Value] && exportsJson.l3Json[this.state.l2Value].length > 0) && <div className="form-group">
+              <select value={this.state.l3Value} className="custom-select" id="l3Json" onChange={this.l3Changed}>
+                <option value="0" disabled>Choose...</option>
+                {this.state.l2Value === "0" || exportsJson.l3Json[this.state.l2Value].map((elm) => {
+                  const label = elm.label.charAt(0).toUpperCase() + elm.label.slice(1);
+                  return (<option key={utils.getUUID()} value={elm.value}>{label}</option>)
+                })}
               </select>
             </div>
+            }
           </div>
         </div>
         <div className="col-8 ">
           <h1 className="">Usage :- </h1>
+          <div>
+            <div class="card">
+              <div class="card-body">
+                {this.state.command}
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-body">
+                {this.state.psScript}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
   }
+
+  l1Changed = (e) => {
+    this.setState({
+      l1Value: e.target.value,
+      l2Value: "0",
+      l3Value: "0"
+    })
+  }
+
+  l2Changed = (e) => {
+    this.setState({
+      l2Value: e.target.value,
+      l3Value: "0"
+    })
+  }
+
+  l3Changed = (e) => {
+    this.setState({
+      l3Value: e.target.value
+    })
+  }
+
 }
 
 export default GitCommands;
